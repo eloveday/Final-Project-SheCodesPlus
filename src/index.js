caa301acdefd39edb72f8a7fa6ecea9f6e69f581
Emily Loveday <emilyloveday91@gmail.com>
2021-07-30 21:33:41 +0100

    Added temperature change function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-function showCurrentWeather(event) {
+function changeForecast(response) {
+  let getCityName = document.querySelector("#city-form");
+  let newCityName = document.querySelector("#city-name");
+  newCityName.innerHTML = getCityName.value;
+
+  let currentTemperature = document.querySelector("#current-temperature");
+  currentTemperature.innerHTML = Math.round(response.data.main.temp);
+}
+
+function getForecast(event) {
   event.preventDefault();
-  let newCityName = document.querySelector("#city-form");
-  let cityName = document.querySelector("#city-name");
-  cityName.innerHTML = newCityName.value;
+  let cityName = document.querySelector("#city-form");
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=b0c4e3d6536928938df05e87e36cbcb5&units=metric`;
+  axios.get(apiURL).then(changeForecast);
 }
 
 let submitButton = document.querySelector("#submit-button");
-submitButton.addEventListener("click", showCurrentWeather);
+submitButton.addEventListener("click", getForecast);
