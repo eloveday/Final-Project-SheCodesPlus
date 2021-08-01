4b3108fbe82ef74e7d370c727e814ca2f72fa9e1
Emily Loveday <emilyloveday91@gmail.com>
2021-08-01 13:25:18 +0100

    Added javascript for celsius and fahrenhieit links

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ function changeForecast(response) {
   let newCityName = document.querySelector("#city-name");
   newCityName.innerHTML = getCityName.value;
 
+  celsiusTemperature = response.data.main.temp;
+
   let currentTemperature = document.querySelector("#current-temperature");
-  currentTemperature.innerHTML = Math.round(response.data.main.temp);
+  currentTemperature.innerHTML = Math.round(celsiusTemperature);
 
   let windSpeed = document.querySelector("#wind-speed");
   windSpeed.innerHTML = Math.round(response.data.wind.speed) + "mph";
@@ -28,6 +30,21 @@ function getForecast(event) {
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=b0c4e3d6536928938df05e87e36cbcb5&units=metric`;
   axios.get(apiURL).then(changeForecast);
 }
+
+function showFahrenheitTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#current-temperature");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function showCelsiusTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#current-temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let submitButton = document.querySelector("#submit-button");
 submitButton.addEventListener("click", getForecast);
@@ -69,3 +86,9 @@ hour.innerHTML = currentHour;
 
 let minute = document.querySelector("#current-minute");
 minute.innerHTML = currentMinute;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsiusTemp);
